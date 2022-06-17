4b9d770bd283cd54181e903478075d1b8e2234a0
cjenkscybercom <christina.jenks@knowit.se>
2022-06-17 10:35:27 +0200

    style: replaced logdetails div with *fluid* container; better practice than div with classes

--- src/components/LogDetails/LogDetails.js
+++ src/components/LogDetails/LogDetails.js
@@ -63,7 +63,7 @@ const LogDetails = ({showGroup, setShowGroup, currentLogEntry, setCurrentLogEntr
         {/* Render only if currentLogRecord is defined, i.e. when user has selected from search result list,
         or if the route is of the type /logs/:id */}
         {currentLogEntry &&
-            <div className="grid-item full-height w-100 px-2" >
+            <Container fluid className="grid-item h-100" >
                 <NavigationButtons selectedLogEntryId={currentLogEntry.id}
                     setCurrentLogEntry={setCurrentLogEntry}/>
                 {/* Site may choose to not support log entry groups */}
@@ -107,7 +107,7 @@ const LogDetails = ({showGroup, setShowGroup, currentLogEntry, setCurrentLogEntr
                         remarkable
                     }}/>
                 }
-            </div>
+            </Container>
         }
         </>
     )
