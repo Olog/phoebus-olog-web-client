df41f92c3a0653c3342d13e0022071d5927ff5c9
cjenkscybercom <christina.jenks@knowit.se>
2022-08-17 11:27:40 +0200

    refactor: advanced search filter inputs are properly labeled/accessible

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -28,6 +28,7 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import customization from '../../utils/customization';
 import { useEffect } from 'react';
+import { InputGroup } from 'react-bootstrap';
 
 /**
  * Component holding search criteria elements, i.e.
@@ -106,126 +107,94 @@ const Filters = ({logbooks, tags, searchParams, setSearchParams, searchPageParam
     return(
         <>
         <Container className="grid-item filters full-height" style={{padding: "8px"}} onKeyDown={onKeyDown} >
-            <Table size="sm" className="search-fields-table">
-                <tbody>
-                    <tr>
-                        <td colSpan="2">Title:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Form.Control size="sm"
-                                type="text"
-                                value={searchParams['title'] || ''}
-                                onChange={(e) => inputChanged(e, 'title')}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">Text:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Form.Control size="sm"
-                                type="text"
-                                value={searchParams['desc'] || ''}
-                                onChange={(e) => inputChanged(e, 'desc')}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">{customization.level}:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Form.Control size="sm"
-                                type="text"
-                                value={searchParams['level'] || ''}
-                                onChange={(e) => inputChanged(e, 'level')}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Logbooks:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Logbooks
-                                logbooks={logbooks}
-                                searchParams={searchParams}
-                                updateLogbookSearchCriteria={logbooks => {inputChangedArray(logbooks, 'logbooks')}}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Tags:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Tags tags={tags}
-                                    searchParams={searchParams}
-                                    updateTagSearchCriteria={tags => {inputChangedArray(tags, 'tags')}}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">Author:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Form.Control size="sm"
-                                type="text"
-                                value={searchParams['owner'] || ''}
-                                onChange={(e) => inputChanged(e, 'owner')}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">Start Time:</td>
-                    </tr>
-                    <tr>
-                        <td style={{width: "100%"}}>
-                        <Form.Control size="sm"
-                            type="text"
-                            value={searchParams['start'] || ''}
-                            onChange={(e) => inputChanged(e, 'start')}/>
-                        </td>
-                        <td><Button size="sm" onClick={() => setShowSelectStartTime(true)}><FaCalendarAlt/></Button></td>
-                    </tr>
-                    <tr>
-                        <td style={{width: "40px"}}>End Time:</td>
-                    </tr>
-                    <tr>
-                        <td style={{width: "100%"}}>
-                        <Form.Control size="sm"
-                            type="text"
-                            value={searchParams['end'] || ''}
-                            onChange={(e) => inputChanged(e, 'end')}/>
-                        </td>
-                        <td><Button size="sm" onClick={() => setShowSelectEndTime(true)}><FaCalendarAlt/></Button></td>
-                    </tr>
-                    <tr>
-                        <td><Form.Check style={{paddingTop: "5px"}}
-                                type='radio'
-                                checked={searchPageParams.sort === 'down'}
-                                label='Sort descending on date'
-                                onChange={(e) => updateSort("down")}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><Form.Check 
-                                type='radio'
-                                label='Sort ascending on date'
-                                checked={searchPageParams.sort === 'up'}
-                                onChange={(e) => updateSort("up")}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">Attachments:</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="2">
-                            <Form.Control size="sm"
-                                type="text"
-                                value={searchParams['attachments'] || ''}
-                                onChange={(e) => inputChanged(e, 'attachments')}/>
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
+            <Form.Group controlId='title'>
+                <Form.Label>Title:</Form.Label>
+                <Form.Control size="sm"
+                    type="text"
+                    value={searchParams['title'] || ''}
+                />
+            </Form.Group>
+            <Form.Group controlId='text'>
+                <Form.Label>Text</Form.Label>
+                <Form.Control size="sm"
+                    type="text"
+                    value={searchParams['desc'] || ''}
+                    onChange={(e) => inputChanged(e, 'desc')}
+                />
+            </Form.Group>
+            <Form.Group controlId='logbooks'>
+                <Form.Label>Logbooks</Form.Label>
+                <Logbooks
+                    logbooks={logbooks}
+                    searchParams={searchParams}
+                    updateLogbookSearchCriteria={logbooks => {inputChangedArray(logbooks, 'logbooks')}}
+                />
+            </Form.Group>
+            <Form.Group controlId='tags'>
+                <Form.Label>Tags</Form.Label>
+                <Tags tags={tags}
+                    searchParams={searchParams}
+                    updateTagSearchCriteria={tags => {inputChangedArray(tags, 'tags')}}
+                />
+            </Form.Group>
+            <Form.Group controlId='author'>
+                <Form.Label>Author</Form.Label>
+                <Form.Control size="sm"
+                    type="text"
+                    value={searchParams['owner'] || ''}
+                    onChange={(e) => inputChanged(e, 'owner')}
+                />
+            </Form.Group>
+            <Form.Group controlId='startTime'>
+                <Form.Label>Start Time</Form.Label>
+                <InputGroup>
+                    <Form.Control size="sm"
+                        type="text"
+                        value={searchParams['start'] || ''}
+                        onChange={(e) => inputChanged(e, 'start')}
+                    />
+                    <InputGroup.Append>
+                        <Button size="sm" onClick={() => setShowSelectStartTime(true)}><FaCalendarAlt/></Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </Form.Group>
+            <Form.Group controlId="endTime">
+                <Form.Label>End Time</Form.Label>
+                <InputGroup>
+                    <Form.Control size="sm"
+                        type="text"
+                        value={searchParams['end'] || ''}
+                        onChange={(e) => inputChanged(e, 'end')}
+                    />
+                    <InputGroup.Append>
+                        <Button size="sm" onClick={() => setShowSelectEndTime(true)}><FaCalendarAlt/></Button>
+                    </InputGroup.Append>
+                </InputGroup>
+            </Form.Group>
+            <Form.Group>
+                <Form.Check style={{paddingTop: "5px"}}
+                    type='radio'
+                    id='sortDescending'
+                    checked={searchPageParams.sort === 'down'}
+                    label='Sort descending on date'
+                    onChange={(e) => updateSort("down")}
+                />
+                <Form.Check 
+                    type='radio'
+                    id='sortAscending'
+                    label='Sort ascending on date'
+                    checked={searchPageParams.sort === 'up'}
+                    onChange={(e) => updateSort("up")}
+                />
+            </Form.Group>
+            <Form.Group controlId='attachments'>
+                <Form.Label>Attachments</Form.Label>
+                <Form.Control size="sm"
+                    type="text"
+                    value={searchParams['attachments'] || ''}
+                    onChange={(e) => inputChanged(e, 'attachments')}
+                />
+            </Form.Group>
         </Container>
         {
         <Modal show={showSelectStartTime} onHide={() => setShowSelectStartTime(false)}>
