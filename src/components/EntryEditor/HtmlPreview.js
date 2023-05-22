cc0909612497f60071a00d2140ae1baca3e11069
cjenkscybercom <christina.jenks@knowit.se>
2023-05-22 10:18:51 +0200

    refactor HtmlPreview from class to functional component

--- src/components/EntryEditor/HtmlPreview.js
+++ src/components/EntryEditor/HtmlPreview.js
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Remarkable } from 'remarkable';
 import imageProcessor from 'utils/image-processor';
 import Button from 'components/shared/Button';
@@ -36,48 +36,44 @@ const StyledHtmlContent = styled(HtmlContent)`
     width: 100%;
 `
 
-class HtmlPreview extends Component{
+const remarkable = new Remarkable('full', {
+    html:         false,        // Enable HTML tags in source
+    xhtmlOut:     false,        // Use '/' to close single tags (<br />)
+    breaks:       false,        // Convert '\n' in paragraphs into <br>
+    langPrefix:   'language-',  // CSS language prefix for fenced blocks
+    linkTarget:   '',           // set target to open link in
+    // Enable some language-neutral replacements + quotes beautification
+    typographer:  false,
+});
 
-    remarkable = new Remarkable('full', {
-        html:         false,        // Enable HTML tags in source
-        xhtmlOut:     false,        // Use '/' to close single tags (<br />)
-        breaks:       false,        // Convert '\n' in paragraphs into <br>
-        langPrefix:   'language-',  // CSS language prefix for fenced blocks
-        linkTarget:   '',           // set target to open link in
-        // Enable some language-neutral replacements + quotes beautification
-        typographer:  false,
-      });
+const HtmlPreview = ({getCommonmarkSrc, getAttachedFiles, showHtmlPreview, setShowHtmlPreview}) => {
 
-    state = {
-        commonmarkSrc: "",
-        innerHtml: {},
+    const [commonmarkSrc, setCommonmarkSrc] = useState("");
+    const [innerHtml, setInnerHtml] = useState("");
+
+    const reset = () => {
+        const source = getCommonmarkSrc();
+        const allAttachedFiles = getAttachedFiles();
+        remarkable.use(imageProcessor, {attachedFiles: allAttachedFiles, setHtmlPreview: true});
+        setInnerHtml(remarkable.render(source));
     }
 
-    reset = () => {
-        var source = this.props.getCommonmarkSrc();
-        var allAttachedFiles = this.props.getAttachedFiles();
-        this.remarkable.use(imageProcessor, {attachedFiles: allAttachedFiles, setHtmlPreview: true});
-        this.setState({innerHtml:  this.remarkable.render(source)  });
-    }
+    return(
+        <StyledModal show={showHtmlPreview}
+            onClose={() => setShowHtmlPreview(false)}
+            onOpen={() => reset()}>
+            <Header onClose={() => setShowHtmlPreview(false)}>
+                <h2>Description Preview</h2>
+            </Header>
+            <Body>
+                <StyledHtmlContent html={{ __html: innerHtml }} />
+            </Body>
+            <Footer>
+                    <Button variant="secondary" onClick={() => setShowHtmlPreview(false)}>OK</Button>
+            </Footer> 
+        </StyledModal>
+    )
 
-    
-    render(){
-        return(
-            <StyledModal show={this.props.showHtmlPreview}
-                onClose={() => this.props.setShowHtmlPreview(false)}
-                onOpen={() => this.reset()}>
-                <Header onClose={() => this.props.setShowHtmlPreview(false)}>
-                    <h2>Description Preview</h2>
-                </Header>
-                <Body>
-                    <StyledHtmlContent html={{ __html: this.state.innerHtml }} />
-                </Body>
-                <Footer>
-                      <Button variant="secondary" onClick={() => this.props.setShowHtmlPreview(false)}>OK</Button>
-                </Footer> 
-            </StyledModal>
-        )
-    }
 }
 
 export default HtmlPreview;
