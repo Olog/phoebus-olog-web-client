1645c54a3aeb8cae5369d66a32e41d678109b88d
cjenkscybercom <christina.jenks@knowit.se>
2023-09-12 17:29:01 +0200

    increased jest timeout

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import { server } from "./mocks/server";
 
+jest.setTimeout(30000);
 beforeAll(() => server.listen());
 beforeEach(() => {
     // Blob support isn't great for jest dom yet
