4158b9f44328688bd6fde50b21ee4bdd6c6ba92d
Max Frederiksen <maxfrederiksen@Maxs-MacBook-Air.local>
2024-10-15 17:18:04 +0200

    add gap between pills vertically

--- src/beta/components/log/LogDetails/MetadataTable.js
+++ src/beta/components/log/LogDetails/MetadataTable.js
@@ -2,32 +2,33 @@ import { Box, Typography, styled } from "@mui/material";
 import React from "react";
 
 const Key = styled(Typography)({
-    fontWeight: "bold",
-    gridColumn: "span / span"
+  fontWeight: "bold",
+  gridColumn: "span / span"
 });
 const Value = styled(Typography)({
-    gridColumn: "span / span"
+  gridColumn: "span / span"
 });
 
-const MetadataTable = styled(({data, KeyProps, ValueProps, className}) => {
+const MetadataTable = styled(({ data, KeyProps, ValueProps, className }) => {
 
-    return (
-        <Box 
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "max-content max-content max-content max-content",
-            columnGap: 2
-          }}
-          className={className}
-        >
-            {Object.entries(data).map(entry => 
-                <React.Fragment key={entry[0]}>
-                    <Key {...KeyProps}>{entry[0]}</Key>
-                    <Value {...ValueProps}>{entry[1]}</Value>
-                </React.Fragment>
-            )}
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "max-content max-content max-content max-content",
+        columnGap: 2,
+        rowGap: 1
+      }}
+      className={className}
+    >
+      {Object.entries(data).map(entry =>
+        <React.Fragment key={entry[0]}>
+          <Key {...KeyProps}>{entry[0]}</Key>
+          <Value {...ValueProps}>{entry[1]}</Value>
+        </React.Fragment>
+      )}
+    </Box>
+  )
 })({})
 
 export default MetadataTable;
