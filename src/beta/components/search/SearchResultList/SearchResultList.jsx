768ff76c76cdf75ad4deb7657e9737672868b9d5
Max Frederiksen <maxfrederiksen@Maxs-MacBook-Air.local>
2024-10-31 11:14:38 +0100

    CSSTUDIO-2728: Add keyboard navigation of entries

--- src/beta/components/search/SearchResultList/SearchResultList.jsx
+++ src/beta/components/search/SearchResultList/SearchResultList.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Divider, Stack, styled } from "@mui/material";
 import { getLogEntryGroupId } from "components/Properties";
 import { SearchResultGroupItem } from "./SearchResultGroupItem/SearchResultGroupItem";
 import { SearchResultSingleItem } from "./SearchResultSingleItem";
 import { sortByCreatedDate } from "components/log/sort";
+import { useKeyPress } from "hooks/useKeyPress";
+import { updateCurrentLogEntry, useCurrentLogEntry } from "features/currentLogEntryReducer";
+import { useDispatch } from "react-redux";
+import useBetaNavigate from "hooks/useBetaNavigate";
 
-export const SearchResultList = styled(({logs, dateDescending, selectedId, onRowClick, className}) => {
+export const SearchResultList = styled(({ logs, dateDescending, className }) => {
+  const dispatch = useDispatch();
+  const navigate = useBetaNavigate();
+
+  const currentLogEntry = useCurrentLogEntry();
+  const currentLogEntryId = Number(currentLogEntry?.id);
+  const arrowUpPressed = useKeyPress('ArrowUp');
+  const arrowDownPressed = useKeyPress('ArrowDown');
 
   const removeSubsequentReplies = (logs) => {
     const visitedGroups = []
     return logs.reduce((res, log) => {
-      if(log.groupId && visitedGroups.includes(log.groupId)) {
+      if (log.groupId && visitedGroups.includes(log.groupId)) {
         return [...res];
       } else {
         visitedGroups.push(log.groupId);
@@ -18,14 +29,46 @@ export const SearchResultList = styled(({logs, dateDescending, selectedId, onRow
       }
     }, []);
   }
+  const logsAndReplies = logs.map(log => ({
+    ...log,
+    groupId: getLogEntryGroupId(log.properties)
+  })).toSorted(sortByCreatedDate(dateDescending))
 
-  const transformedLogs = removeSubsequentReplies(
-    logs.map(log => ({
-      ...log, 
-      groupId: getLogEntryGroupId(log.properties)
-    }))
-    .toSorted(sortByCreatedDate(dateDescending))
+  const logsNoReplies = removeSubsequentReplies(
+    logsAndReplies
   );
+
+  const navigateToEntry = (log) => {
+    dispatch(updateCurrentLogEntry(log));
+    navigate(`/logs/${log.id}`);
+  }
+
+  const keyboardNavigate = (nextEntryId) => {
+    const nextEntry = logsAndReplies.find(log => log.id === nextEntryId);
+    if (nextEntry) {
+      navigateToEntry(nextEntry);
+    }
+  }
+
+  const setDefaultLogEntry = () => {
+    if (!currentLogEntryId) {
+      dispatch(updateCurrentLogEntry(logsNoReplies[0]));
+    }
+  }
+
+  useEffect(() => {
+    if (arrowUpPressed) {
+      setDefaultLogEntry();
+      keyboardNavigate(currentLogEntryId + 1);
+    }
+  }, [arrowUpPressed]);
+
+  useEffect(() => {
+    if (arrowDownPressed) {
+      setDefaultLogEntry();
+      keyboardNavigate(currentLogEntryId - 1);
+    }
+  }, [arrowDownPressed]);
 
   return (
     <Stack
@@ -33,11 +76,11 @@ export const SearchResultList = styled(({logs, dateDescending, selectedId, onRow
       className={`SearchResultList ${className}`}
       divider={<Divider flexItem />}
     >
-      {transformedLogs?.map(log => {
-        if(log.groupId) {
-          return <SearchResultGroupItem key={log.id} log={log} selectedId={selectedId} onClick={onRowClick} dateDescending={dateDescending} />
+      {logsNoReplies?.map(log => {
+        if (log.groupId) {
+          return <SearchResultGroupItem key={log.id} log={log} selectedId={currentLogEntryId} onClick={navigateToEntry} dateDescending={dateDescending} />
         } else {
-          return <SearchResultSingleItem key={log.id} log={log} selected={`${selectedId}` === `${log.id}`} onClick={onRowClick} />
+          return <SearchResultSingleItem key={log.id} log={log} selected={`${currentLogEntryId}` === `${log.id}`} onClick={navigateToEntry} />
         }
       })}
     </Stack>
