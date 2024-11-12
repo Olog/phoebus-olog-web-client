821a8512325d46eb838f7f82448403f5d1cf0962
georgweiss <georg.weiss@ess.eu>
2024-11-12 18:16:32 +0100

    Update install/build script for Github action

--- scripts/install-dependencies-and-build.sh
+++ scripts/install-dependencies-and-build.sh
@@ -3,5 +3,7 @@
 # Install dependencies and build for later steps
 npm ci
 
+export REACT_APP_LEVEL_VALUES=[\"Normal\",\"Shift Start\",\"Shift End\",\"Fault\",\"Beam Loss\",\"Beam Configuration\",\"Crew\",\"Expert Intervention Call\",\"Incident\"]
+
 # Verify the project is buildable in prod
 docker build -t test-build-image . -f test.Dockerfile
