1f55a6f31dec49d602858d74291874b36c788635
Max Frederiksen <maxfrederiksen@Maxs-MacBook-Air.local>
2024-11-18 14:47:51 +0100

    Add form validation for Entry Type select field

--- src/components/log/EntryEditor/EntryEditor.js
+++ src/components/log/EntryEditor/EntryEditor.js
@@ -15,72 +15,84 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-import { useEffect } from 'react';
-import TextInput from 'components/shared/input/TextInput';
-import { useRef } from 'react';
-import { Button, Stack, Typography } from '@mui/material';
-import Description from './Description';
-import LogbooksMultiSelect from 'components/shared/input/managed/LogbooksMultiSelect';
-import TagsMultiSelect from 'components/shared/input/managed/TagsMultiSelect';
-import EntryTypeSelect from 'components/shared/input/managed/EntryTypeSelect';
-import PropertyCollectionInput from 'components/shared/input/managed/PropertyCollectionInput';
+import { useEffect } from "react";
+import TextInput from "components/shared/input/TextInput";
+import { useRef } from "react";
+import { Button, Stack, Typography } from "@mui/material";
+import Description from "./Description";
+import LogbooksMultiSelect from "components/shared/input/managed/LogbooksMultiSelect";
+import TagsMultiSelect from "components/shared/input/managed/TagsMultiSelect";
+import EntryTypeSelect from "components/shared/input/managed/EntryTypeSelect";
+import PropertyCollectionInput from "components/shared/input/managed/PropertyCollectionInput";
 
-export const EntryEditor = ({form, title, onSubmit, submitDisabled, attachmentsDisabled}) => {
+export const EntryEditor = ({
+  form,
+  title,
+  onSubmit,
+  submitDisabled,
+  attachmentsDisabled,
+}) => {
+  const topElem = useRef();
+  const { control, handleSubmit, formState } = form;
 
-    const topElem = useRef();
-    const { control, handleSubmit, formState } = form;
+  // Scroll to top if there are field errors
+  useEffect(() => {
+    if (Object.keys(formState.errors).length > 0) {
+      if (
+        topElem.current &&
+        typeof topElem.current.scrollIntoView === "function"
+      ) {
+        topElem.current.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [formState]);
 
-    // Scroll to top if there are field errors
-    useEffect(() => {
-        if(Object.keys(formState.errors).length > 0) {
-            if(topElem.current && typeof topElem.current.scrollIntoView === 'function') {
-                topElem.current.scrollIntoView({ behavior: "smooth" });
-            }
-        }
-    }, [formState])
-
-    return (
-        <>
-            <Stack gap={1} overflow="scroll" padding={1} >
-                <Typography component="h1" variant="h3" >{title}</Typography>
-                <Stack component="form" onSubmit={handleSubmit(onSubmit)} gap={1} >
-                    <span ref={topElem}></span>
-                    <LogbooksMultiSelect 
-                        control={control}
-                        rules={{
-                            validate: {
-                                notEmpty: val => val?.length > 0 || 'Select at least one logbook'
-                            }
-                        }}
-                    />
-                    <TagsMultiSelect 
-                        control={control}
-                    />
-                    <EntryTypeSelect 
-                        control={control}
-                    />
-                    <TextInput 
-                        name='title'
-                        label='Title'
-                        control={control}
-                        defaultValue=''
-                        rules={{
-                            required: {
-                                value: true,
-                                message: 'Please specify a title.'
-                            }
-                        }}
-                    />
-                    <Description 
-                        form={form}
-                        attachmentsDisabled={attachmentsDisabled}
-                    />
-                    <PropertyCollectionInput 
-                        control={control}
-                    />
-                    <Button type='submit' variant="contained" disabled={submitDisabled}>Submit</Button>
-                </Stack>
-            </Stack>
-        </>
-    );
-}
+  return (
+    <>
+      <Stack gap={1} overflow="scroll" padding={1}>
+        <Typography component="h1" variant="h3">
+          {title}
+        </Typography>
+        <Stack component="form" onSubmit={handleSubmit(onSubmit)} gap={1}>
+          <span ref={topElem}></span>
+          <LogbooksMultiSelect
+            control={control}
+            rules={{
+              validate: {
+                notEmpty: (val) =>
+                  val?.length > 0 || "Select at least one logbook",
+              },
+            }}
+          />
+          <TagsMultiSelect control={control} />
+          <EntryTypeSelect
+            rules={{
+              validate: {
+                notEmpty: (val) =>
+                  val?.length > 0 || "Please select an Entry Type",
+              },
+            }}
+            control={control}
+          />
+          <TextInput
+            name="title"
+            label="Title"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: {
+                value: true,
+                message: "Please specify a title.",
+              },
+            }}
+          />
+          <Description form={form} attachmentsDisabled={attachmentsDisabled} />
+          <PropertyCollectionInput control={control} />
+          <Button type="submit" variant="contained" disabled={submitDisabled}>
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
