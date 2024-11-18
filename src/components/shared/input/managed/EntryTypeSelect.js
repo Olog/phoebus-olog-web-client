708cd7676837dfa171c51223f727dc8eadeb9a10
Max Frederiksen <maxfrederiksen@Maxs-MacBook-Air.local>
2024-11-18 15:12:31 +0100

    JSON.parse error handling + avoid unecessary re-renders logic

--- src/components/shared/input/managed/EntryTypeSelect.js
+++ src/components/shared/input/managed/EntryTypeSelect.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MultiSelect from "../MultiSelect";
 import { styled } from "@mui/material";
 import customization from "config/customization";
 
-const EntryTypeSelect = styled(({control, className, ...props}) => {
-    return (
-        <MultiSelect 
-            className={className}
-            name='level'
-            label={customization.level ?? "Entry Type"}
-            control={control}
-            defaultValue={customization.defaultLevel}
-            options={JSON.parse(customization.levelValues)}
-            {...props}
-        />
-    )
+const EntryTypeSelect = styled(({ control, className, ...props }) => {
+  const [levelValues, setLevelValues] = useState([]);
+
+  useEffect(() => {
+    try {
+      setLevelValues(JSON.parse(customization.levelValues));
+    } catch (e) {
+      console.error("Failed to parse level values:", e);
+      return [];
+    }
+  }, []);
+
+  return (
+    <MultiSelect
+      className={className}
+      name="level"
+      label={customization.level ?? "Entry Type"}
+      control={control}
+      defaultValue={customization.defaultLevel}
+      options={levelValues}
+      {...props}
+    />
+  );
 })({});
 
 export default EntryTypeSelect;
