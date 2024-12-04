6677a839b84b1137fb04e59183c9bfa8ff33aa57
Max Frederiksen <maxfrederiksen@Maxs-MacBook-Air.local>
2024-12-04 14:46:31 +0100

    Implement new markdown-it and rename CommonMark component and folder

--- src/components/LogDetails/LogEntrySingleView.js
+++ src/components/LogDetails/LogEntrySingleView.js
@@ -16,111 +16,120 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-import React from 'react';
-import Properties from '../Properties/Properties';
-import LogDetailsMetaData from './LogDetailsMetaData';
-import Collapse from 'components/shared/Collapse';
-import CommonmarkPreview from 'components/shared/CommonmarkPreview';
-import customization from 'config/customization';
-import AttachmentImage, { isImage } from 'components/Attachment/AttachmentImage';
-import { Box, Divider, Typography, styled } from '@mui/material';
+import React from "react";
+import Properties from "../Properties/Properties";
+import LogDetailsMetaData from "./LogDetailsMetaData";
+import Collapse from "components/shared/Collapse";
+import CommonMark from "components/shared/CommonMark";
+import customization from "config/customization";
+import AttachmentImage, {
+  isImage,
+} from "components/Attachment/AttachmentImage";
+import { Box, Divider, Typography, styled } from "@mui/material";
 
 const Container = styled("div")`
-    padding: 0.5rem;
-    height: 100%;
-    overflow: auto;
-`
+  padding: 0.5rem;
+  height: 100%;
+  overflow: auto;
+`;
 
 const LogTitle = styled("div")`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 
-    &, & h2 {
-        font-size: 1.4rem;
-        font-weight: normal;
-    }
-`
+  &,
+  & h2 {
+    font-size: 1.4rem;
+    font-weight: normal;
+  }
+`;
 
-const StyledCommonmarkPreview = styled(CommonmarkPreview)`
-    width: 100%;
-    padding-top: 0.5rem;
-    padding-bottom: 1rem;
-    wordWrap: break-word;
-    font-size: 1.2rem;
-    overflow: auto;
-`
+const StyledCommonmarkPreview = styled(CommonMark)`
+  width: 100%;
+  padding-top: 0.5rem;
+  padding-bottom: 1rem;
+  wordwrap: break-word;
+  font-size: 1.2rem;
+  overflow: auto;
+`;
 
 const StyledAttachmentImage = styled(AttachmentImage)`
-    width: 100%;
+  width: 100%;
 
-    &:hover {
-        cursor: pointer;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const LogEntrySingleView = ({ currentLogEntry, className }) => {
+  const attachments = currentLogEntry.attachments.map((attachment, index) => {
+    const url = `${customization.APP_BASE_URL}/attachment/` + attachment.id;
+
+    if (isImage(attachment)) {
+      return (
+        <Box key={index}>
+          <StyledAttachmentImage
+            onClick={() => {
+              let w = window.open("", attachment.filename);
+              w.document.open();
+              w.document.write(
+                "<html><head><title>" + attachment.filename + "</title></head>"
+              );
+              w.document.write(
+                "<body><p><img src='" + url + "'></p></body></html>"
+              );
+              w.document.close();
+            }}
+            attachment={attachment}
+          />
+        </Box>
+      );
+    } else {
+      return (
+        <Box key={index}>
+          <a target="_blank" rel="noopener noreferrer" href={url}>
+            {attachment.filename}
+          </a>
+        </Box>
+      );
     }
-`
+  });
 
-const LogEntrySingleView = ({currentLogEntry, className}) => {
-    
-    const attachments = currentLogEntry.attachments.map((attachment, index) => {
-        const url = `${customization.APP_BASE_URL}/attachment/` + attachment.id;
-        
-        if(isImage(attachment)) {
-            return (
-                <Box key={index}>
-                    <StyledAttachmentImage 
-                        onClick={() => {
-                                let w = window.open("", attachment.filename);
-                                w.document.open();
-                                w.document.write('<html><head><title>' + attachment.filename + '</title></head>');
-                                w.document.write('<body><p><img src=\'' + url + '\'></p></body></html>');
-                                w.document.close();
-                            }
-                        }
-                        attachment={attachment}
-                    />
-                </Box>
-            )
-        } else {
-            return (
-                <Box key={index}>
-                    <a target="_blank" rel="noopener noreferrer" href={url}>
-                        {attachment.filename}
-                    </a>
-                </Box>
-            )
-        }
-    });
-    
-    const properties = currentLogEntry.properties
-        .filter(property => property.name !== 'Log Entry Group')
-        .map((property, index) => <Properties key={index} property={property}/>);
+  const properties = currentLogEntry.properties
+    .filter((property) => property.name !== "Log Entry Group")
+    .map((property, index) => <Properties key={index} property={property} />);
 
-    return(
-        <Container className={className}>
-            <LogTitle>
-                <Typography component="h2" variant="h2">{currentLogEntry.title}</Typography>
-                <Typography component="span">{currentLogEntry.id}</Typography>
-            </LogTitle>
-            <Divider sx={{marginY: 1}} />
-            <LogDetailsMetaData currentLogRecord={currentLogEntry}/>
-            <Divider sx={{marginY: 1}} />
-            <StyledCommonmarkPreview commonmarkSrc={currentLogEntry.source} imageUrlPrefix={customization.APP_BASE_URL + "/"} />
-            <Collapse title='Attachments'>
-                {currentLogEntry.attachments.length > 0 
-                    ? <Box>
-                        {attachments}
-                    </Box>
-                    : <Typography>No Attachments</Typography> 
-                }
-            </Collapse>
-            <Collapse title='Properties'>
-                {properties?.length > 0
-                    ? properties 
-                    : <Typography>No Properties</Typography> 
-                }
-            </Collapse>
-        </Container>
-    );
-    
-}
+  return (
+    <Container className={className}>
+      <LogTitle>
+        <Typography component="h2" variant="h2">
+          {currentLogEntry.title}
+        </Typography>
+        <Typography component="span">{currentLogEntry.id}</Typography>
+      </LogTitle>
+      <Divider sx={{ marginY: 1 }} />
+      <LogDetailsMetaData currentLogRecord={currentLogEntry} />
+      <Divider sx={{ marginY: 1 }} />
+      <StyledCommonmarkPreview
+        commonmarkSrc={currentLogEntry.source}
+        imageUrlPrefix={customization.APP_BASE_URL + "/"}
+      />
+      <Collapse title="Attachments">
+        {currentLogEntry.attachments.length > 0 ? (
+          <Box>{attachments}</Box>
+        ) : (
+          <Typography>No Attachments</Typography>
+        )}
+      </Collapse>
+      <Collapse title="Properties">
+        {properties?.length > 0 ? (
+          properties
+        ) : (
+          <Typography>No Properties</Typography>
+        )}
+      </Collapse>
+    </Container>
+  );
+};
 
 export default LogEntrySingleView;
---
+++ src/stories/CommonMark.stories.js
@@ -0,0 +1,88 @@
+import CommonMark from "components/shared/CommonMark";
+
+export default {
+  title: "Common/CommonMark",
+  component: CommonMark,
+};
+
+const source = `# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+Regular *Italic* **Bold** ***Both***
+
+[Wikipedia Link Example](https://www.wikipedia.org/)
+
+Regular Image:
+
+![React Logo](./logo192.png)
+
+Resized Image:
+
+![Stretched React Logo](./logo192.png){width=200 height=50}
+
+BlockQuotes:
+> *Lorem ipsum dolor sit amet consectetur adipisicing 
+elit. Voluptas, quasi ut voluptates quo necessitatibus similique 
+blanditiis illo, repellat enim error quidem qui quam, ab recusandae!*
+
+Horizontal Rule: 
+
+---
+
+Unordered List:
+
+* Red
+    * Fruit
+        * Apples
+        * Peppers
+            * Sweet
+            * Spicy
+    * Objects
+        * Fire Hydrant 
+* Green
+* Blue
+
+Ordered List:
+
+1. Intro
+    1. Hook
+    1. Summary
+1. Body
+    1. Supporting Point
+        1. More Details
+        1. More Details
+            1. Even More Details
+        1. More Details
+    1. Supporting Point
+    1. Supporting Point
+1. Conclusion
+    1. Summary
+    1. Closing
+
+This is \`inline code\` whereas the below is: 
+
+\`\`\`
+// Block Code
+const sayHello = (name) => {
+    console.log(\`Hello, $\{name\}!\`);
+};
+sayHello("World");
+\`\`\`
+
+Table:
+
+| Table Header 1| Table Header 2|
+|---------------|---------------|
+| Table Cell 11 | Table Cell 12 |
+| Table Cell 21 | Table Cell 22 |
+`;
+
+export const Default = {
+  args: {
+    commonmarkSrc: source,
+  },
+};
