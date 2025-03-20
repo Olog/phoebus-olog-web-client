6c62ba2172adb042ad31f1f9275b05ca119cecb1
Max Frederiksen <max.frederiksen@ess.eu>
2025-03-20 10:46:09 +0100

    CSSTUDIO-3093: Comment out legacy routes

--- src/providers/RouterProvider.jsx
+++ src/providers/RouterProvider.jsx
@@ -4,9 +4,9 @@ import {
 } from "react-router-dom";
 import BetaApp from "beta/views/BetaApp";
 import SearchView from "beta/views/SearchView";
-import { LogEntriesView } from "components/LogEntriesView";
+// import { LogEntriesView } from "components/LogEntriesView";
 import { AppErrorBoundary } from "components/shared/error/ErrorBoundary";
-import App from "views/App";
+// import App from "views/App";
 import CreateLogView from "views/CreateLogView";
 import EditLogView from "views/EditLogView";
 import { HelpView } from "views/HelpView";
@@ -16,48 +16,48 @@ import ReplyLogView from "views/ReplyLogView";
 import { TestErrorView } from "views/TestErrorView";
 
 export const routes = [
-  {
-    path: "/legacy",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <LogEntriesView />
-      },
-      {
-        path: "logs/:id",
-        element: <LogEntriesView />
-      },
-      {
-        path: "logs/create",
-        element: <CreateLogView />
-      },
-      {
-        path: "logs/:id/reply",
-        element: <ReplyLogView />
-      },
-      {
-        path: "logs/:id/edit",
-        element: <EditLogView />
-      },
-      {
-        path: "logs/:id/history",
-        element: <LogHistoryView />
-      },
-      {
-        path: "error-test",
-        element: <TestErrorView />
-      },
-      {
-        path: "help",
-        element: <HelpView />
-      },
-      {
-        path: "*",
-        element: <NotFoundView />
-      }
-    ]
-  },
+  // {
+  //   path: "/legacy",
+  //   element: <App />,
+  //   children: [
+  //     {
+  //       path: "",
+  //       element: <LogEntriesView />
+  //     },
+  //     {
+  //       path: "logs/:id",
+  //       element: <LogEntriesView />
+  //     },
+  //     {
+  //       path: "logs/create",
+  //       element: <CreateLogView />
+  //     },
+  //     {
+  //       path: "logs/:id/reply",
+  //       element: <ReplyLogView />
+  //     },
+  //     {
+  //       path: "logs/:id/edit",
+  //       element: <EditLogView />
+  //     },
+  //     {
+  //       path: "logs/:id/history",
+  //       element: <LogHistoryView />
+  //     },
+  //     {
+  //       path: "error-test",
+  //       element: <TestErrorView />
+  //     },
+  //     {
+  //       path: "help",
+  //       element: <HelpView />
+  //     },
+  //     {
+  //       path: "*",
+  //       element: <NotFoundView />
+  //     }
+  //   ]
+  // },
   {
     path: "/",
     element: <BetaApp />,
