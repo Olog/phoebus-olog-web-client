bd944ed82af74eabe117c5b44f08bf41433f7852
Max Frederiksen <max.frederiksen@ess.eu>
2025-06-12 14:32:54 +0200

    CSSTUDIO-3298: Fix file image rendering and style

--- src/components/Attachment/AttachmentImage.jsx
+++ src/components/Attachment/AttachmentImage.jsx
@@ -16,7 +16,7 @@ export const isImage = (attachment) => {
   }
 };
 
-export const FileImage = ({ alt, className, ...props }) => {
+export const FileImage = ({ attachment, alt, className, ...props }) => {
   return (
     <Stack
       fontSize="5rem"
@@ -27,35 +27,50 @@ export const FileImage = ({ alt, className, ...props }) => {
       className={className}
       {...props}
     >
-      <Box
-        className="file-overlay"
-        sx={{
-          display: "none",
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-          background: "white",
-          opacity: 0.6,
-          zIndex: 1
-        }}
-      />
-      <DownloadIcon
-        className="file-overlay"
-        sx={{
-          color: "gray",
-          width: "22px",
-          height: "22px",
-          position: "absolute",
-          left: "33px",
-          bottom: "25px",
-          zIndex: 2
-        }}
-      />
-      <InsertDriveFileIcon
-        titleAccess={"file: " + alt}
-        fontSize="inherit"
-        sx={{ perserveAspectRatio: "none" }}
-      />
+      {isImage(attachment) ? (
+        <img
+          src={attachment.url}
+          alt={alt}
+          style={{
+            width: "100%",
+            objectFit: "contain",
+            overflow: "hidden"
+          }}
+        />
+      ) : (
+        <>
+          <Box
+            className="file-overlay"
+            sx={{
+              display: "none",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              background: "white",
+              opacity: 0.6,
+              zIndex: 1
+            }}
+          />
+          {!isLocalFile(attachment) && (
+            <DownloadIcon
+              className="file-overlay"
+              sx={{
+                color: "gray",
+                width: "22px",
+                height: "22px",
+                position: "absolute",
+                left: "33px",
+                bottom: "25px",
+                zIndex: 2
+              }}
+            />
+          )}
+          <InsertDriveFileIcon
+            titleAccess={"file: " + alt}
+            fontSize="inherit"
+          />
+        </>
+      )}
     </Stack>
   );
 };
