e2de240eae72ca72dcced7ce5d4c2de9ff06aec5
Bolch, Tim <tibolch@uni-mainz.de>
2025-09-11 12:51:51 +0200

    Squashed commit (branch) bump-all-versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,6 @@ const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
