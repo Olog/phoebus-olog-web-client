52979e5f011298ac1c0b9eaa7431aa2b0ebcd44f
Tim Bolch <tibolch@uni-mainz.de>
2025-09-17 15:48:16 +0200

    updated eslint.config.js to resemble .eslintsr

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import globals from "globals";
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -8,16 +9,6 @@ import prettier from 'eslint-plugin-prettier'
 
 export default [
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
     plugins: {
       react: react,
       'react-hooks': reactHooks,
@@ -27,37 +18,27 @@ export default [
       cypress: cypress,
       prettier: prettier,
     },
-    rules: {
-      'react-refresh/only-export-components': 'warn',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'import/order': [
-        'error',
-        {
-          groups: [
-            'external',
-            'index',
-            'sibling',
-            'parent',
-            'internal',
-            'builtin',
-            'object',
-            'type'
-          ]
-        }
-      ],
-      'no-console': [
-        'error',
-        {
-          allow: ['warn', 'error', 'info', 'debug']
-        }
-      ],
-      curly: ['error', 'all'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      // like env in .eslintrc
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     settings: {
       react: {
         version: 'detect',
+      },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
       },
       'import/resolver': {
         alias: {
@@ -73,12 +54,139 @@ export default [
             ['lib', './src/lib'],
             ['mocks', './src/mocks'],
             ['stories', './src/stories'],
-            ['test-utils', './src/test-utils']
+            ['test-utils', './src/test-utils'],
           ],
-          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
-        }
-      }
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
     },
-    ignores: ['node_modules/', 'dist/', '.git/', '*.min.js']
-  }
+
+    rules: {
+      // like extends in .eslintrc
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      ...typescriptEslint.configs.recommended.rules,
+      ...cypress.configs.recommended.rules,
+      ...prettier.configs.recommended.rules,
+      ...importPlugin.configs.recommended.rules,
+      ...importPlugin.configs.typescript.rules,
+      // React
+      'react/prop-types': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-boolean-value': 'warn',
+      'react/no-danger': 'warn',
+      'react/no-multi-comp': [
+        'warn',
+        {
+          ignoreStateless: true,
+        },
+      ],
+      'react/prefer-es6-class': 'warn',
+      'react/self-closing-comp': 'error',
+      'react-refresh/only-export-components': 'warn',
+
+      // Import
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'external',
+            'index',
+            'sibling',
+            'parent',
+            'internal',
+            'builtin',
+            'object',
+            'type',
+          ],
+        },
+      ],
+      'import/no-named-as-default': 'error',
+      'import/no-named-as-default-member': 'error',
+      'import/no-unresolved': [
+        'error',
+        { caseSensitive: false, commonjs: true, amd: true },
+      ],
+      'import/no-duplicates': 'error',
+
+      // General
+      'consistent-this': ['error', 'self'],
+      'default-case': 'error',
+      'dot-notation': 'error',
+      eqeqeq: [
+        'error',
+        'always',
+        {
+          null: 'ignore',
+        },
+      ],
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error', 'info', 'debug'],
+        },
+      ],
+      'no-eval': 'error',
+      'no-implied-eval': 'error',
+      'no-lone-blocks': 'error',
+      'no-nested-ternary': 'error',
+      'no-object-constructor': 'error',
+      'no-octal-escape': 'error',
+      'no-script-url': 'error',
+      'no-self-compare': 'error',
+      'no-sequences': 'error',
+      'no-undef-init': 'error',
+      'no-unused-expressions': 'error',
+      'operator-assignment': ['error', 'always'],
+      quotes: [
+        'error',
+        'double',
+        {
+          avoidEscape: true,
+        },
+      ],
+      radix: 'error',
+      yoda: 'error',
+      curly: ['error', 'all'],
+    },
+    ignores: ['node_modules/', 'dist/', '.git/', '*.min.js'],
+  },
+  // Overrides
+  {
+    files: ['**/*.stories.*'],
+    rules: {
+      'import/no-anonymous-default-export': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    files: ['**/mocks/mockAPI.js', '**/index.js', 'cypress.config.js'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    files: [
+      '**/*.spec.js',
+      '**/*.spec.jsx',
+      '**/*.spec.ts',
+      '**/*.spec.tsx',
+      '*.test.js',
+      'setupTests.js',
+      '*.tsx',
+      '*.ts',
+      '*.jsx',
+      '*.js',
+      '*.cy.js',
+    ],
+  },
 ]
