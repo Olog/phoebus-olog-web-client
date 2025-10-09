8f302dd96bc654224aa9ecd6956e0c995f9e3499
Max Frederiksen <max.frederiksen@ess.eu>
2025-10-09 15:30:29 +0200

    Fix websocket bugs

--- src/views/EditLogView.jsx
+++ src/views/EditLogView.jsx
@@ -1,15 +1,37 @@
 import { useParams } from "react-router-dom";
+import { Alert, LinearProgress } from "@mui/material";
 import { EditLog } from "components/log/EditLog";
-import LogContainer from "components/log/LogContainer";
+import { ologApi } from "src/api/ologApi";
 
 const EditLogView = () => {
   const { id } = useParams();
-
-  return (
-    <LogContainer
-      id={id}
-      renderLog={({ log }) => <EditLog {...{ log }} />}
-    />
+  const {
+    data: log,
+    isLoading,
+    error
+  } = ologApi.endpoints.getLog.useQuery(
+    { id, noInvalidate: true },
+    {
+      skip: false
+    }
   );
+
+  if (isLoading) {
+    return <LinearProgress sx={{ width: "100%" }} />;
+  }
+
+  if (error) {
+    return (
+      <Alert
+        severity="error"
+        sx={{ m: 2 }}
+      >
+        Error loading log entry {id}
+      </Alert>
+    );
+  }
+
+  return <EditLog log={log} />;
 };
+
 export default EditLogView;
