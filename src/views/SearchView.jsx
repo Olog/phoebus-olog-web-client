cfe2537d71389deb64d4424bd9cc021e926c23b9
Bolch, Tim <tibolch@uni-mainz.de>
2025-10-16 11:16:57 +0200

    update use re-resizable package instead

--- src/views/SearchView.jsx
+++ src/views/SearchView.jsx
@@ -1,113 +1,55 @@
-import { useRef, useState, useEffect, useCallback } from "react";
-import { Box, Divider, styled, useTheme, useMediaQuery } from "@mui/material";
+import { Box, Divider, styled } from "@mui/material";
 import { useParams } from "react-router-dom";
 import LogDetailsContainer from "src/components/log/LogDetails/LogDetailsContainer";
 import { SearchResults } from "components/search";
+import { Resizable } from "re-resizable";
 
-const ContentView = styled(Box, {
-  shouldForwardProp: (prop) => prop !== "leftFlex" && prop !== "rightFlex"
-})(({ theme, leftFlex, rightFlex }) => ({
-  display: "grid",
-  gridTemplateColumns: `minmax(0,0) minmax(150px, ${leftFlex}fr) minmax(0,4px) minmax(250px, ${rightFlex}fr)`,
+const ContentView = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
   height: "100%",
   [theme.breakpoints.down("md")]: {
     display: "flex",
     flexDirection: "column-reverse",
-    "& > *": { flex: 1, minWidth: 0 }
+    height: "100%",
+    width: "auto",
+    minWidth: 0,
+    "& > div": {
+      flex: 1
+    },
+    "& > hr": {
+      height: "auto",
+      minHeight: "auto"
+    }
   }
 }));
 
-function SearchView() {
+const SearchView = styled(({ className }) => {
   const { id } = useParams();
-  const theme = useTheme();
-  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
-  const [leftFlex, setLeftFlex] = useState(1.25);
-  const rightFlex = 3.24 - leftFlex;
+  return (
+    <Box height="100%">
+      <ContentView className={`SearchView ${className}`}>
+        <Divider
+          sx={{ borderColor: "#E2E8EE" }}
+          orientation="vertical"
+        />
+        <Resizable minWidth="200px" maxWidth="90%" defaultSize={{ width: "38.4%" }}>
+          <SearchResults />
+        </Resizable>
+        <Divider
+          sx={{ borderColor: "#E2E8EE" }}
+          orientation="vertical"
+        />
+          <LogDetailsContainer id={id} />
 
-  const { onMouseMove, onMouseUp, containerRef, onMouseDown } = useLayoutRatio(
-    leftFlex,
-    setLeftFlex
+      </ContentView>
+    </Box>
   );
-
-  useEffect(() => {
-    return () => {
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", onMouseUp);
-    };
-  }, [onMouseMove, onMouseUp]);
-
-  return isDesktop ? (
-    <ContentView
-      ref={containerRef}
-      leftFlex={leftFlex}
-      rightFlex={rightFlex}
-    >
-      <Divider sx={{ bgcolor: "#E2E8EE" }} />
-      <SearchResults />
-      <Divider
-        onMouseDown={onMouseDown}
-        sx={{ bgcolor: "#ffffffff", cursor: "col-resize" }}
-      />
-      <LogDetailsContainer id={id} />
-    </ContentView>
-  ) : (
-    <ContentView
-      leftFlex={leftFlex}
-      rightFlex={rightFlex}
-    >
-      <Divider sx={{ borderColor: "#E2E8EE", orientation: "horizontal" }} />
-      <SearchResults />
-      <Divider sx={{ borderColor: "#E2E8EE", orientation: "horizontal" }} />
-      <LogDetailsContainer id={id} />
-    </ContentView>
-  );
-}
+})({
+  "& > *": {
+    minWidth: 0
+  }
+});
 
 export default SearchView;
-
-function useLayoutRatio(leftFlex, setLeftFlex) {
-  const containerRef = useRef(null);
-  const dragging = useRef(false);
-  const startX = useRef(0);
-  const startFlex = useRef(0);
-
-  const clamp = (v, min, max) => Math.min(Math.max(v, min), max);
-
-  const onMouseMove = useCallback(
-    (e) => {
-      if (!dragging.current) {
-        return;
-      }
-      const dx = e.clientX - startX.current;
-      const { width } = containerRef.current.getBoundingClientRect();
-      const flexArea = width - 16;
-      const deltaFlex = (dx / flexArea) * 4;
-      setLeftFlex(clamp(startFlex.current + deltaFlex, 1, 3));
-    },
-    [setLeftFlex]
-  );
-
-  const onMouseUp = useCallback(() => {
-    dragging.current = false;
-    document.removeEventListener("mousemove", onMouseMove);
-    document.removeEventListener("mouseup", onMouseUp);
-  }, [onMouseMove]);
-
-  const onMouseDown = useCallback(
-    (e) => {
-      if (e.button !== 0) {
-        return;
-      }
-      e.preventDefault();
-      dragging.current = true;
-      startX.current = e.clientX;
-      startFlex.current = leftFlex;
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
-    },
-    [leftFlex, onMouseMove, onMouseUp]
-  );
-
-  return { onMouseMove, onMouseUp, containerRef, onMouseDown };
-}
