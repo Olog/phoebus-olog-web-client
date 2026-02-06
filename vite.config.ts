88af12af1229e2f0ba1deb5af3523b67ff305ba5
lcaouen <loic.caouen@cea.fr>
2026-02-06 15:13:52 +0100

    fix https://github.com/Olog/phoebus-olog-web-client/issues/304
    correct the routes and allow relative navigation depending on the base
    base is now configured in .env which more generic

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,45 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: "build",
-    emptyOutDir: true
-  },
-  plugins: [svgr(), react()],
-  resolve: {
-    alias: {
-      src: "/src",
-      config: "/src/config",
-      components: "/src/components",
-      views: "/src/views",
-      hooks: "/src/hooks",
-      providers: "/src/providers",
-      features: "/src/features",
-      api: "/src/api",
-      lib: "/src/lib",
-      mocks: "/src/mocks",
-      stories: "/src/stories",
-      "test-utils": "/src/test-utils"
-    }
-  },
-  test: {
-    setupFiles: "./src/setupTests.ts",
-    environment: "jsdom",
-    coverage: {
-      reporter: ["lcov", "text"]
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_APP_ENDPOINT,
+    server: {
+      port: 3000,
+      open: true
     },
-    outputFile: "coverage/sonar-report.xml"
-  }
-});
+    build: {
+      outDir: "build",
+      emptyOutDir: true
+    },
+    plugins: [svgr(), react()],
+    resolve: {
+      alias: {
+        src: "/src",
+        config: "/src/config",
+        components: "/src/components",
+        views: "/src/views",
+        hooks: "/src/hooks",
+        providers: "/src/providers",
+        features: "/src/features",
+        api: "/src/api",
+        lib: "/src/lib",
+        mocks: "/src/mocks",
+        stories: "/src/stories",
+        "test-utils": "/src/test-utils"
+      }
+    },
+    test: {
+      setupFiles: "./src/setupTests.ts",
+      environment: "jsdom",
+      coverage: {
+        reporter: ["lcov", "text"]
+      },
+      outputFile: "coverage/sonar-report.xml"
+    }
+  };
+})
