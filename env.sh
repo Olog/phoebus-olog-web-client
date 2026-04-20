395a0c95bf6b5c25e4e83cfc35668420e8b1b076
Max Frederiksen <max.frederiksen@ess.eu>
2026-04-20 16:53:06 +0200

    Modified env.sh

--- env.sh
+++ env.sh
@@ -4,6 +4,7 @@ env | grep "^VITE_APP_" | while IFS='=' read -r key value; do
     echo "  • Replacing ${key} → ${value}"
 
     # Use find and sed to replace the variable in all files within the directory
+    # First replace /KEY/ (with surrounding slashes), then plain KEY
     find "/usr/share/nginx/html/" -type f \
-        -exec sed -i "s|${key}|${value}|g" {} +
+        -exec sed -i -e "s|/${key}/|${value}|g" -e "s|${key}|${value}|g" {} +
 done
